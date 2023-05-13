b4a2f0822547890304ee6a05ac98aa98806c0af3
Kiet <kietquang240703@gmail.com>
2023-05-13 11:18:03 +0700

    add file demo

---
+++ demo/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Players from './Players';
+
+
+import Navigation from './components/Navigation';
+function App(){
+ return(
+   <div className="App">
+    <Navigation/> 
+   <Players/>
+      </div>
+
+ );
+
+}
+export default App;
