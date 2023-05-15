2dde7f41bdcbe63091b1d0e5d0a7e96200b39de3
Kiet <kietquang240703@gmail.com>
2023-05-15 16:44:20 +0700

    Functional Components

--- demo/src/components/Navigation.js
+++ demo/src/components/Navigation.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react'
-export default class Navigation extends Component {
-    render() {
-        return (
-            <div className='menu'>
-                <nav>
-                <ul className='header'>
-                        <li><a className='active' href='#home'>Home</a></li>
-                        <li><a href='#news'>News</a></li>
-                        <li><a href='#about'>About</a></li>
-                        <li><a href='#contact'>Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
-        )
-    }
+import React from 'react'
+export default function Navigation() {
+  return (
+       <div className='menu'>
+    <nav>
+  <ul className='header'>
+      <li><a className='active' href='#home'>Home</a></li>
+      <li><a href='#news'>News</a></li>
+      <li><a href='#about'>About</a></li>
+      <li><a href='#contact'>Contact</a></li>
+  </ul>
+</nav>
+ </div>
+  )
 }
-
